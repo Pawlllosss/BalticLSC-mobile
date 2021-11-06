56b6cd48164d6c4d16f559eabdb426b95e79893d
Pawlllosss <oczadlypawe@gmail.com>
2021-11-06 14:04:31 +0100

    Implemented logout action. Styles modification.

--- BalticLSC/src/main/java/pl/oczadly/baltic/lsc/android/MainActivity.kt
+++ BalticLSC/src/main/java/pl/oczadly/baltic/lsc/android/MainActivity.kt
@@ -1,15 +1,28 @@
 package pl.oczadly.baltic.lsc.android
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.asLiveData
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
+import kotlin.coroutines.CoroutineContext
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import pl.oczadly.baltic.lsc.UserState
+import pl.oczadly.baltic.lsc.android.view.LoginView
 import pl.oczadly.baltic.lsc.android.view.ViewPageAdapter
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+
+    private val job = Job()
+
+    override val coroutineContext: CoroutineContext
+        get() = job
 
     companion object {
 
@@ -43,5 +56,22 @@ class MainActivity : AppCompatActivity() {
         val inflater = menuInflater
         inflater.inflate(R.menu.action_bar_settings, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.logut_action -> {
+            launch(Dispatchers.Main) {
+                AndroidUserState(applicationContext!!).clear()
+
+                Intent(baseContext, LoginView::class.java).also {
+                    it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(it)
+                }
+            }
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
     }
 }
