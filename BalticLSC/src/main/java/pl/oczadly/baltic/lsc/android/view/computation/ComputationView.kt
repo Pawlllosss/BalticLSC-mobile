a47310ac65580aecaeaf96c49d0f4400c731ce9b
Pawel Oczadly <oczadlypawe@gmail.com>
2021-12-02 19:37:46 +0100

    Completed simplifying association of task with app

--- BalticLSC/src/main/java/pl/oczadly/baltic/lsc/android/view/computation/ComputationView.kt
+++ BalticLSC/src/main/java/pl/oczadly/baltic/lsc/android/view/computation/ComputationView.kt
@@ -14,12 +14,13 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import pl.oczadly.baltic.lsc.android.MainActivity
 import pl.oczadly.baltic.lsc.android.R
+import pl.oczadly.baltic.lsc.android.view.app.converter.AppListItemEntityConverter
 import pl.oczadly.baltic.lsc.android.view.computation.adapter.ComputationTaskGroupAdapter
 import pl.oczadly.baltic.lsc.android.view.computation.entity.ComputationTaskEntity
 import pl.oczadly.baltic.lsc.android.view.computation.entity.ComputationTaskGroup
 import pl.oczadly.baltic.lsc.app.AppApi
-import pl.oczadly.baltic.lsc.app.dto.AppShelfItem
 import pl.oczadly.baltic.lsc.app.dto.list.AppListItem
+import pl.oczadly.baltic.lsc.app.dto.list.AppRelease
 import pl.oczadly.baltic.lsc.computation.ComputationApi
 import pl.oczadly.baltic.lsc.computation.dto.Task
 import pl.oczadly.baltic.lsc.lazyPromise
@@ -51,6 +52,7 @@ class ComputationView : Fragment(), CoroutineScope {
             }
         }
     }
+    private val appListItemEntityConverter = AppListItemEntityConverter()
 
     override val coroutineContext: CoroutineContext
         get() = job
@@ -68,46 +70,50 @@ class ComputationView : Fragment(), CoroutineScope {
             val applications = apps.await()
             val computationTasks = tasks.await()
 
-            val tasksAndAppByAppName = groupTasksAndAppByAppName(applications, computationTasks)
-            val computationTaskGroups = createComputationTaskGroups(tasksAndAppByAppName)
+            val tasksByApp: Map<AppListItem, List<Task>> =
+                groupTasksByApp(applications, computationTasks)
+            val computationTaskGroups = createComputationTaskGroups(tasksByApp)
 
             val recyclerView = view.findViewById<RecyclerView>(R.id.computation_recycler_view)
             recyclerView.adapter = ComputationTaskGroupAdapter(computationTaskGroups)
         }
     }
 
-    private fun groupTasksAndAppByAppName(
+    private fun groupTasksByApp(
         applications: List<AppListItem>,
         computationTasks: List<Task>
-    ): Map<String, List<Map.Entry<Task, AppListItem>>> {
-        val applicationByAppVersionUid: Map<String, AppListItem> = applications.flatMap { app -> app.releases.map { it.uid to app } }.toMap()
+    ): Map<AppListItem, List<Task>> {
+        val applicationByAppVersionUid: Map<String, AppListItem> =
+            applications.flatMap { app -> app.releases.map { it.uid to app } }.toMap()
         val computationTasksAndApps: Map<Task, AppListItem> =
             computationTasks.filter { applicationByAppVersionUid.containsKey(it.releaseUid) }
                 .map { it to applicationByAppVersionUid[it.releaseUid]!! }
                 .toMap()
         return computationTasksAndApps.entries
-            .groupBy({ it.value.name }, { it })
+            .groupBy({ it.value }, { it.key })
     }
 
-    private fun createComputationTaskGroups(tasksAndAppByAppName: Map<String, List<Map.Entry<Task, AppShelfItem>>>) =
-        tasksAndAppByAppName.map {
+    private fun createComputationTaskGroups(tasksByApp: Map<AppListItem, List<Task>>) =
+        tasksByApp.map { taskByApp ->
+            val appReleaseByUid = taskByApp.key.releases.map { it.uid to it }.toMap()
             ComputationTaskGroup(
-                it.key,
-                createComputationTasks(it)
+                appListItemEntityConverter.convertFromAppListItemDTO(taskByApp.key),
+                createComputationTasks(taskByApp.value, appReleaseByUid)
             )
         }
 
-    private fun createComputationTasks(it: Map.Entry<String, List<Map.Entry<Task, AppShelfItem>>>) =
-        it.value.map {
-            val task = it.key
-            val app = it.value
+    private fun createComputationTasks(
+        tasks: List<Task>,
+        appReleaseByUid: Map<String, AppRelease>
+    ) =
+        tasks.map {
             ComputationTaskEntity(
-                task.parameters.taskName,
-                app.version,
-                task.start,
-                task.finish,
-                task.status,
-                task.parameters.priority
+                it.parameters.taskName,
+                appReleaseByUid[it.releaseUid]?.version ?: "",
+                it.start,
+                it.finish,
+                it.status,
+                it.parameters.priority
             )
         }
 }
