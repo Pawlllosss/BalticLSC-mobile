c1a788ca6e06d011b7415118e36c0c31f70105c6
Pawlllosss <oczadlypawe@gmail.com>
2021-12-04 22:11:35 +0100

    Added abort task view

--- BalticLSC/src/main/java/pl/oczadly/baltic/lsc/android/view/computation/entity/ComputationTaskEntity.kt
+++ BalticLSC/src/main/java/pl/oczadly/baltic/lsc/android/view/computation/entity/ComputationTaskEntity.kt
@@ -1,9 +1,11 @@
 package pl.oczadly.baltic.lsc.android.view.computation.entity
 
+import java.io.Serializable
 import kotlinx.datetime.Instant
 import pl.oczadly.baltic.lsc.computation.dto.ComputationStatus
 
 data class ComputationTaskEntity(
+    val uid: String,
     val name: String,
     val version: String,
     val startTime: Instant,
