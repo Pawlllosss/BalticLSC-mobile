e0e5b8ba3e4ce789965080f7e95f7b442e934ec9
Pawlllosss <oczadlypawe@gmail.com>
2021-12-05 20:43:38 +0100

    Creating programmatically spinners that contains data type uid

--- BalticLSC/src/main/java/pl/oczadly/baltic/lsc/android/view/computation/activity/ComputationTaskStart.kt
+++ BalticLSC/src/main/java/pl/oczadly/baltic/lsc/android/view/computation/activity/ComputationTaskStart.kt
@@ -1,7 +1,11 @@
 package pl.oczadly.baltic.lsc.android.view.computation.activity
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.coroutines.CoroutineContext
 import kotlinx.coroutines.CoroutineScope
@@ -9,6 +13,7 @@ import kotlinx.coroutines.Job
 import pl.oczadly.baltic.lsc.android.MainActivity
 import pl.oczadly.baltic.lsc.android.R
 import pl.oczadly.baltic.lsc.android.view.app.entity.DatasetPinEntity
+import pl.oczadly.baltic.lsc.app.dto.dataset.DatasetBinding
 import pl.oczadly.baltic.lsc.computation.ComputationApi
 
 class ComputationTaskStart : AppCompatActivity(), CoroutineScope {
@@ -22,19 +27,25 @@ class ComputationTaskStart : AppCompatActivity(), CoroutineScope {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val taskNameIntent = intent.getStringExtra("computationTaskName")
-        val datasetPinsIntent =
+        val taskName = intent.getStringExtra("computationTaskName")
+        val datasetPins =
             intent.getSerializableExtra("datasetPins") as? ArrayList<DatasetPinEntity>
-        if (taskNameIntent == null || datasetPinsIntent == null) {
+        if (taskName == null || datasetPins == null) {
             finish()
         } else {
             setContentView(R.layout.activity_computation_task_start)
             setSupportActionBar(findViewById(R.id.toolbar))
 
-            val taskName = taskNameIntent!!
-            val datasetPins = datasetPinsIntent!!
+            findViewById<TextView>(R.id.computation_task_start_task_name_text_view).text = taskName
 
-            // TODO: create and populate spinners with proper value
+            val requiredLinearLayout =
+                findViewById<LinearLayout>(R.id.computation_task_start_required_linear_layout)
+            val requiredDatasets = datasetPins.filter { it.binding == DatasetBinding.REQUIRED }
+            addDatasetSpinnersToLayout(requiredDatasets, requiredLinearLayout)
+            val providedLinearLayout =
+                findViewById<LinearLayout>(R.id.computation_task_start_provided_linear_layout)
+            val providedDatasets = datasetPins.filter { it.binding == DatasetBinding.PROVIDED }
+            addDatasetSpinnersToLayout(providedDatasets, providedLinearLayout)
 
             findViewById<Button>(R.id.computation_task_start_create_button)
                 .setOnClickListener {
@@ -46,6 +57,26 @@ class ComputationTaskStart : AppCompatActivity(), CoroutineScope {
                 .setOnClickListener {
                     finish()
                 }
+        }
+    }
+
+    private fun addDatasetSpinnersToLayout(
+        datasets: List<DatasetPinEntity>,
+        layout: LinearLayout
+    ) {
+        datasets.forEach {
+            val textView = TextView(applicationContext)
+            textView.text = it.name
+            val spinner = Spinner(applicationContext)
+            // TODO: fetch data suitable for dataTypeUid
+            spinner.adapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_dropdown_item,
+                listOf(it.dataTypeUid)
+            )
+
+            layout.addView(textView)
+            layout.addView(spinner)
         }
     }
 }
