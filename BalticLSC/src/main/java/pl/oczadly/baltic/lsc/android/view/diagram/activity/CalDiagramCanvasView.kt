0fee500b798252e1e4def51d2b1db3b76b7f9eab
Pawel Oczadly <oczadlypawe@gmail.com>
2022-04-29 15:14:05 +0200

    Using diagram to draw circles

--- BalticLSC/src/main/java/pl/oczadly/baltic/lsc/android/view/diagram/activity/CalDiagramCanvasView.kt
+++ BalticLSC/src/main/java/pl/oczadly/baltic/lsc/android/view/diagram/activity/CalDiagramCanvasView.kt
@@ -6,8 +6,11 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import pl.oczadly.baltic.lsc.android.view.diagram.entity.DiagramEntity
 
 class CalDiagramCanvasView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+
+    var diagram = DiagramEntity.emptyDiagram
 
     // TODO: add diagram property and iterate over it in onDraw https://stackoverflow.com/questions/9233800/how-to-get-current-canvas
 
@@ -20,10 +23,14 @@ class CalDiagramCanvasView(context: Context?, attrs: AttributeSet?) : View(conte
         drawPaint.strokeJoin = Paint.Join.ROUND
         drawPaint.strokeCap = Paint.Cap.ROUND
 
-        canvas?.drawCircle(50f, 50f, 20f, drawPaint)
-        drawPaint.color = Color.GREEN
-        canvas?.drawCircle(50f, 150f, 20f, drawPaint)
-        drawPaint.color = Color.BLUE
-        canvas?.drawCircle(50f, 250f, 20f, drawPaint)
+        diagram.elements.forEach {
+            canvas?.drawCircle(it.x.toFloat(), it.y.toFloat(), it.width.toFloat(), drawPaint)
+        }
+
+//        canvas?.drawCircle(50f, 50f, 20f, drawPaint)
+//        drawPaint.color = Color.GREEN
+//        canvas?.drawCircle(50f, 150f, 20f, drawPaint)
+//        drawPaint.color = Color.BLUE
+//        canvas?.drawCircle(50f, 250f, 20f, drawPaint)
     }
 }
